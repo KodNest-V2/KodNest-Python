d3a4ebb0661d7d3c5fc4fb6da62dfb645f574c7c
Syna <169091063+Synabegum@users.noreply.github.com>
2026-08-13 21:51:36 +0530

    print statements

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,1 +1,5 @@
 #write you code here
+print("Welcome to the Python Track.")
+print("My name is Syna")
+print("My goal is to become a python full stack developer")
+print("I selected Python because I want to build web applications using data, automation and AI")
